5727f603d8e0c555b6cca2c5d62314f2f6185549
Afranio Alves <afranio777alves@gmail.com>
2023-02-10 10:52:22 +0100

    secção dos artigos adicionado

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,6 @@
 import Menu from "../../components/menu";
+import Detail from "../../components/detail";
+import './index.css'
 
 const Home = ()=>{
 
@@ -8,7 +10,56 @@ const Home = ()=>{
                 <header>
                     <Menu />
                 </header>
-                <h1>This is Home Page</h1>
+                
+                <section className="section-info">
+
+                    <div>
+                        <h1 className="description">
+                            Encontre o café perfeito <br />
+                            para qualquer hora do dia
+                        </h1>
+                        <p className="detail">
+                            Com o Coffee Delivery você recebe seu café onde estiver, a <br />
+                            qualquer hora.
+                        </p>
+
+                        <div className="box-details">
+                            <Detail 
+                                colour="#C47F17"
+                                description="Compra simples e segura"
+                                icon = "fa-cart-shopping"
+                            />
+                             <Detail 
+                                colour="#574F4D"
+                                description="Embalagem mantém o café intacto"
+                                icon = "fa-box"
+                            />
+                             <Detail 
+                                colour="#DBAC2C"
+                                description="Entrega rápida e rastreada"
+                                icon = "fa-stopwatch"
+                            />
+                             <Detail 
+                                colour="#8047F8"
+                                description="O café chega fresquinho até você"
+                                icon = "fa-mug-hot"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="/images/Imagem.svg" alt="coffee image" className="coffe-image" />
+                    </div>
+                </section>
+
+                <section className="section-articles">
+                    <h1>Nossos cafés</h1>
+                    
+                    <div className="box-articles">
+
+                    </div>
+                </section>
+
             </main>
         </>
     )
